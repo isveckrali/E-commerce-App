70768a49d23df0f5b7ec7e8d6797fb55e6537b47
Mehmet Can Seyhan <13541515.mehmetcan.seyhan@gmail.com>
2021-08-23 12:24:43 +0200

    Initial Commit

---
+++ E-commerce/E_commerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_commerceApp.swift
+//  E-commerce
+//
+//  Created by Mehmet Can Seyhan on 2021-08-23.
+//
+
+import SwiftUI
+
+@main
+struct E_commerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
